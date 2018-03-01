d86897f270467043b860ab11ada74b46293665db
lichenyang <lichenyanggloria@sina.com>
2018-03-01 10:06:33 +0800

    refactor func

--- main.go
+++ main.go
@@ -12,7 +12,8 @@ import (
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	// users
-	router.HandleFunc("/users", controller.UserHandler).Methods("GET", "POST")
+	router.HandleFunc("/users", controller.GetUserHandler).Methods("GET")
+	router.HandleFunc("/users", controller.CreateUserHandler).Methods("POST")
 	// relationships
 	router.HandleFunc("/users/{user_id:[0-9]+}/relationships", controller.GetRelationshipHandler).Methods("GET")
 	router.HandleFunc("/users/{user_id:[0-9]+}/relationships/{other_user_id:[0-9]+}", controller.UpdateRelationshipHandler).Methods("PUT")
